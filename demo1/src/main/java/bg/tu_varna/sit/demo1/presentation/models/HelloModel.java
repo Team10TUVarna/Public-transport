6ca53166a6b82401e220dbe30066b101a6a7cd6b
Team10_TUVarna <kenankemalow00@gmail.com>
2021-11-18 13:38:32 +0200

    demo project
    demo project

---
+++ demo1/src/main/java/bg/tu_varna/sit/demo1/presentation/models/HelloModel.java
@@ -0,0 +1,16 @@
+package bg.tu_varna.sit.demo1.presentation.models;
+
+public class HelloModel
+{
+    private final String welcomeMessage;
+
+    public HelloModel()
+    {
+        this.welcomeMessage="Hello SIT I am JavaFX Project";
+    }
+    public String getWelcomeMessage()
+        {
+            return welcomeMessage;
+        }
+
+}
