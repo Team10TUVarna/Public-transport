574dbf350e59fa130f675032490ec8cec737229a
Team10_TUVarna <92537154+Team10TUVarna@users.noreply.github.com>
2022-01-25 13:32:29 +0200

    deleted files

--- demo1/src/main/java/bg/tu_varna/sit/demo1/common/Constants.java
+++
@@ -1,9 +0,0 @@
-package bg.tu_varna.sit.demo1.common;
-
-public class Constants
-{
-    public static class View
-    {
-        public static final String HELLO_VIEW="/bg/tu_varna/sit/demo1/presentations.views/hello-view.fxml";
-    }
-}
