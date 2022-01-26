049d29b845a4eea7a1f659cdb3b3aec7618a6193
Team10_TUVarna <kenankemalow00@gmail.com>
2022-01-26 23:35:12 +0200

    Views and controllers

--- OOP_Project_demo/src/main/java/bg/tu_varna/sit/oop_project_demo/presentation/controllers/AdminLoginController.java
+++ OOP_Project_demo/src/main/java/bg/tu_varna/sit/oop_project_demo/presentation/controllers/AdminLoginController.java
@@ -1,13 +1,39 @@
 package bg.tu_varna.sit.oop_project_demo.presentation.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class AdminLoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminLoginController implements Initializable {
+
+    @FXML
+    private Button backButton;
 
     public AdminLoginController(Stage stage) {
+
+    }
+
+    @FXML
+    public void goBack(ActionEvent actionEvent)
+    {
+
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
